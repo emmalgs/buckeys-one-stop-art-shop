b7a2d8ecb49c1284d10a437b804464b29476d95d
Emma Gerigscott <elgerig@gmail.com>
2023-07-14 14:14:31 -0700

    revert back to firestore

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore';
 
 interface FirebaseConfig {
   apiKey: string;
@@ -24,6 +24,6 @@ const firebaseConfig: FirebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const db = getFirestore(app);
 
 export default db;
