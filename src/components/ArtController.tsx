abca39a8527898c8501f62ba7f8ccd211f93c1d7
Emma Gerigscott <elgerig@gmail.com>
2023-07-14 13:20:46 -0700

    WIP: attempt at timer

--- src/components/ArtController.tsx
+++ src/components/ArtController.tsx
@@ -5,19 +5,28 @@ import { useState, useEffect } from "react";
 import { query, collection, where, limit, getDocs, onSnapshot } from "firebase/firestore"
 import db from "./../firebase";
 
-
 function ArtController() {
   const [cartVisible, setCartVisible] = useState<boolean>(false);
   const [homeVisible, setHomeVisible] = useState<boolean>(true);
-  // const [countdown, setCountdown] = userState<number>(0);
+  const [countdown, setCountdown] = useState(0);
   const [currentArt, setCurrentArt] = useState({});
-  // const [artList, setArtList] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown((prevCountdown: number) => prevCountdown - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   useEffect(() => {
     const unSubscribe = onSnapshot(
-      query(collection(db, "art"), limit(1)),
+      query(collection(db, "art"), where("available", "==", true), limit(1)),
       (collectionSnapshot) => {
         const artwork = collectionSnapshot.docs[0].data();
+        setCountdown(artwork.timer)
         setCurrentArt(artwork);
       },
       (error) => {
@@ -26,7 +35,9 @@ function ArtController() {
     );
   
     return () => unSubscribe();
-  }, []);
+  }, [countdown]);
+  
+
 
   const handleBuyClick = () => {
     setCartVisible(true);
