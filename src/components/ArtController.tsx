b9295be460e4d29570329b472ee02238505717e7
Emma Gerigscott <elgerig@gmail.com>
2023-07-14 17:01:18 -0700

    BUGFIX: set current art with snapshot values instead of state

--- src/components/ArtController.tsx
+++ src/components/ArtController.tsx
@@ -5,30 +5,39 @@ import { useState, useEffect } from "react";
 import { ref, onValue } from "firebase/database";
 import db from "./../firebase";
 
+interface ArtObj {
+  title: string;
+  description: string;
+  price: string;
+  imageUrl: string;
+  available: boolean;
+}
+
 function ArtController() {
   const [cartVisible, setCartVisible] = useState<boolean>(false);
   const [homeVisible, setHomeVisible] = useState<boolean>(true);
-  const [allArt, setAllArt] = useState([]);
+  const [allArt, setAllArt] = useState<ArtObj[]>([]);
   const [currentArt, setCurrentArt] = useState({});
   // const [countdown, setCountdown] = useState(0);
 
   useEffect(() => {
     const artdb = ref(db, 'art/');
     const unSubscribe = onValue(
-      artdb, (snapshot) => {
-      const artworks = [];
-      const data = snapshot.val();
+      artdb, (snapshot: import("firebase/database").DataSnapshot) => {
+      const artworks: ArtObj[] = [];
+      const data = snapshot.val() as Record<string, ArtObj>;
       Object.keys(data).forEach((art) => {
         artworks.push(data[art]);
       });
       setAllArt(artworks)
-      setCurrentArt(allArt[0])
+      setCurrentArt(artworks[0])
     },
     (error) => {
       console.log(error);
     });
     return () => unSubscribe();
-  }, [allArt]);
+  }, []);
+  
 
   // useEffect(() => {
   //   const interval = setInterval(() => {
