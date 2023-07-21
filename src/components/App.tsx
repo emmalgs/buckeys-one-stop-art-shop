49f8dc959d17d00397de62c5f15e326e36efc48e
Emma Gerigscott <elgerig@gmail.com>
2023-07-21 08:47:26 -0700

    add routes for admin and art

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,17 @@
-import ArtController from './user/ArtController'
-import AdminControl from './admin/AdminControl';
-import './../App.css'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ArtController from "./user/ArtController";
+import AdminControl from "./admin/AdminControl";
+import "./../App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const cartVisible = false;
   return (
-    <>
-      <ArtController cartVisible={cartVisible}/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/admin" element={<AdminControl />} />
+        <Route path="/" element={<ArtController />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
