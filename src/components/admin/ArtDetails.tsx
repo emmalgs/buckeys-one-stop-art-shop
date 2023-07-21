5539414db507cf4350d89db5c626a5a6f6640719
Emma Gerigscott <elgerig@gmail.com>
2023-07-21 14:25:07 -0700

    add delete props to button

--- src/components/admin/ArtDetails.tsx
+++ src/components/admin/ArtDetails.tsx
@@ -1,5 +1,6 @@
 interface DetailProps {
   selection: ArtObj;
+  deleteArt: (id: string) => void
 }
 
 interface ArtObj {
@@ -11,17 +12,21 @@ interface ArtObj {
 }
 
 function ArtDetails(props: DetailProps) {
-  const art: ArtObj = props.selection
+  const art: ArtObj = props.selection;
   return (
     <div className="art-details">
       <h2>{art.title}</h2>
       <p>{art.description}</p>
       <p>${parseInt(art.price).toFixed(2)}</p>
-      <p><img src={art.imageUrl} /></p>
-      <button>Delete</button>
-      <button>Edit</button>
+      <p>
+        <img src={art.imageUrl} />
+      </p>
+      <div className="art-btns">
+        <button onClick={() => props.deleteArt(art.id)}>Delete</button>
+        <button>Edit</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ArtDetails;
