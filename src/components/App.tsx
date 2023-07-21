ed0d9068cac6bf7369cb62e01aaa675f9d7ccc07
Emma Gerigscott <elgerig@gmail.com>
2023-07-21 08:44:06 -0700

    install react router dom

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,7 @@
-import ArtController from './ArtController'
+import ArtController from './user/ArtController'
+import AdminControl from './admin/AdminControl';
 import './../App.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   const cartVisible = false;
