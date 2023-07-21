6c2a5e512584fde434009f896300cc95f29cb7aa
Emma Gerigscott <elgerig@gmail.com>
2023-07-21 08:18:05 -0700

    create admin component

---
+++ src/components/admin/AdminControl.tsx
@@ -0,0 +1,35 @@
+import React, { userState, useEffect, useState } from 'react';
+import { ref, onValue } from "firebase/database";
+import { db, auth } from "../../firebase";
+
+interface ArtObj {
+  title: string;
+  description: string;
+  price: string;
+  imageUrl: string;
+  available: boolean;
+}
+
+function AdminControl() {
+  const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
+  const [artQueue, setArtQueue] = useState<ArtObj[]>([]);
+  const [selectedArt, setSelectedArt] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const artdb = ref(db, 'art/');
+    const unSubscribe = onValue(
+      artdb, (snapshot: import("firebase/database").DataSnapshot) => {
+      const artworks: ArtObj[] = [];
+      const data = snapshot.val() as Record<string, ArtObj>;
+      Object.keys(data).forEach((art) => {
+        artworks.push(data[art]);
+      });
+      setArtQueue(artworks)
+    },
+    (error) => {
+      console.log(error);
+    });
+    return () => unSubscribe();
+  }, []);
+}
