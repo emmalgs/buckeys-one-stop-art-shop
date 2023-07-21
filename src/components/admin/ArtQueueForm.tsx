3179f3c58b2e7801277f742e8b93ba200e138b60
Emma Gerigscott <elgerig@gmail.com>
2023-07-21 10:58:04 -0700

    add image file and upload to storage, eslint error on async function submitArt

--- src/components/admin/ArtQueueForm.tsx
+++ src/components/admin/ArtQueueForm.tsx
@@ -1,3 +1,5 @@
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
 interface FormProps {
   addSomeArt: (arg0: ArtObj) => void;
 }
@@ -6,11 +8,29 @@ interface ArtObj {
   title: string;
   description: string;
   price: string;
+  imageUrl: string;
 }
+let uploadedImg: File | null = null
 
 function ArtQueueForm(props: FormProps) {
-  function submitArt(e: React.FormEvent<HTMLFormElement>) {
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      uploadedImg = event.target.files[0];
+    }
+    console.log(uploadedImg)
+  }
+  async function submitArt(e: React.FormEvent<HTMLFormElement>): Promise<void> {
     e.preventDefault();
+    if (!uploadedImg) {
+      console.error('No image selected');
+      return;
+    }
+    const storage = getStorage();
+    const storageRef = ref(storage, `images/${uploadedImg.name}`);
+    await uploadBytes(storageRef, uploadedImg);
+
+    const imageURL = await getDownloadURL(storageRef);
+
     const target = e.target as typeof e.target & {
       title: { value: string};
       description: {value: string};
@@ -20,15 +40,20 @@ function ArtQueueForm(props: FormProps) {
       title: target.title.value,
       description: target.description.value,
       price: target.price.value,
+      imageUrl: imageURL
     }
     props.addSomeArt(art);
   }
+
   return (
     <div>
       <form onSubmit={submitArt}>
         <input type="text" name="title" placeholder="title" />
         <textarea name="description" placeholder="description of art piece" />
         <input type="number" name="price" placeholder="price" />
+        <label>Upload Image: 
+          <input type="file" accept="image/*" onChange={handleImageChange}/>
+        </label>
         <button>Add Art</button>
       </form>
     </div>
