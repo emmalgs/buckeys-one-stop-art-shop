cffbce967c22ca7f0e86ea6f6d63079b273c1a7d
Emma Gerigscott <elgerig@gmail.com>
2023-07-21 10:04:51 -0700

    add view queue state

--- src/components/admin/AdminControl.tsx
+++ src/components/admin/AdminControl.tsx
@@ -5,6 +5,7 @@ import AdminLogin from './AdminLogin';
 import ArtQueueForm from './ArtQueueForm';
 import AdminHeader from './AdminHeader';
 import AdminLogout from './AdminLogout';
+import QueueList from './QueueList';
 
 interface ArtObj {
   title: string;
@@ -20,6 +21,7 @@ function AdminControl() {
   // const [selectedArt, setSelectedArt] = useState(null);
   const [loginView, setLoginView] = useState(false);
   const [logoutView, setLogoutView] = useState(false);
+  const [viewQueue, setViewQueue] = useState(false);
 
   useEffect(() => {
     const artdb = ref(db, 'art/');
@@ -42,12 +44,32 @@ function AdminControl() {
     setLoginView(true);
     setFormVisibleOnPage(false);
     setLogoutView(false);
+    setViewQueue(false);
+    setFormVisibleOnPage(false);
   }
 
   const handleLogoutViewClick = () => {
     setLoginView(false);
     setFormVisibleOnPage(false);
     setLogoutView(true);
+    setViewQueue(false);
+    setFormVisibleOnPage(false);
+  }
+
+  const handleViewQueueClick = () => {
+    setLoginView(false);
+    setFormVisibleOnPage(false);
+    setLogoutView(false);
+    setViewQueue(true);
+    setFormVisibleOnPage(false);
+  }
+
+  const handleAddArtClick = () => {
+    setLoginView(false);
+    setFormVisibleOnPage(false);
+    setLogoutView(false);
+    setViewQueue(false);
+    setFormVisibleOnPage(true);
   }
 
 
@@ -60,7 +82,11 @@ function AdminControl() {
     }
     return (
       <div className='admin-body'>
-        <AdminHeader loginClick={handleLoginViewClick} logoutViewClick={handleLogoutViewClick}/>
+        <AdminHeader 
+          loginClick={handleLoginViewClick} 
+          logoutViewClick={handleLogoutViewClick} 
+          viewQueueClick={handleViewQueueClick}
+          addArtClick={handleAddArtClick} />
         {currentView}
       </div>
     )
@@ -68,12 +94,18 @@ function AdminControl() {
     let currentView = null;
     if (logoutView) {
       currentView = <AdminLogout />
-    } else {
+    } else if (formVisibleOnPage) {
       currentView = <ArtQueueForm />
+    } else if (viewQueue) {
+      currentView = <QueueList allArt={artQueue} />
     }
     return (
       <div className='admin-body'>
-        <AdminHeader loginClick={handleLoginViewClick}logoutViewClick={handleLogoutViewClick} />
+        <AdminHeader 
+          loginClick={handleLoginViewClick}
+          logoutViewClick={handleLogoutViewClick}
+          viewQueueClick={handleViewQueueClick}
+          addArtClick={handleAddArtClick} />
         {currentView}
       </div>
     )
