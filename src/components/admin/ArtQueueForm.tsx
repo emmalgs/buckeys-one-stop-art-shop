0b60a06b3b3bbe6e4ac0b85bdc77afb7e2b21685
Emma Gerigscott <elgerig@gmail.com>
2023-07-21 10:19:50 -0700

    basic add art function

--- src/components/admin/ArtQueueForm.tsx
+++ src/components/admin/ArtQueueForm.tsx
@@ -1,12 +1,35 @@
+interface FormProps {
+  addSomeArt: (arg0: ArtObj) => void;
+}
 
-function ArtQueueForm() {
+interface ArtObj {
+  title: string;
+  description: string;
+  price: string;
+}
+
+function ArtQueueForm(props: FormProps) {
+  function submitArt(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      title: { value: string};
+      description: {value: string};
+      price: { value: string };
+    }
+    const art = {
+      title: target.title.value,
+      description: target.description.value,
+      price: target.price.value,
+    }
+    props.addSomeArt(art);
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={submitArt}>
         <input type="text" name="title" placeholder="title" />
         <textarea name="description" placeholder="description of art piece" />
         <input type="number" name="price" placeholder="price" />
-        <input type="number" name="timer" placeholder="days up for sale" />
+        <button>Add Art</button>
       </form>
     </div>
   )
