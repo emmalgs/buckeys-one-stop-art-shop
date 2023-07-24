7820d830dcac700cc6419e69a4a3227814283455
Emma Gerigscott <elgerig@gmail.com>
2023-07-24 08:57:46 -0700

    add on click function to add to queue

--- src/components/admin/AllArtList.tsx
+++ src/components/admin/AllArtList.tsx
@@ -1,6 +1,7 @@
 interface QueueProps {
   allArt: Array<ArtObj>;
   onArtClick: (arg0: string) => void;
+  onAddToQueueClick: (id: string) => void;
 }
 
 interface ArtObj {
@@ -16,19 +17,32 @@ function AllArtList(props: QueueProps) {
     <div className="all-art-list">
       {props.allArt.map((art) => {
         return (
-          <div className="art-queue-item" key={art.id} id={art.id} onClick={() => props.onArtClick(art.id)}>
-            <p>Title: <em>{art.title}</em></p>
-            <p>Description: <em>{art.description}</em></p>
-            <p>Price : ${parseInt(art.price).toFixed(2)}</p>
-            <p><img src={art.imageUrl} /></p>
-            {/* Add button conditional if art already exists in queue then it should read Added to Queue */}
-            <button>Add To Queue</button>
+          <div key={art.id}>
+            <div
+              className="art-queue-item"
+              id={art.id}
+              onClick={() => props.onArtClick(art.id)}
+            >
+              <p>
+                Title: <em>{art.title}</em>
+              </p>
+              <p>
+                Description: <em>{art.description}</em>
+              </p>
+              <p>Price : ${parseInt(art.price).toFixed(2)}</p>
+              <p>
+                <img src={art.imageUrl} />
+              </p>
+              {/* Add button conditional if art already exists in queue then it should read Added to Queue */}
+            </div>
+            <button onClick={() => props.onAddToQueueClick(art.id)}>
+              Add To Queue
+            </button>
           </div>
-          );
-        })
-      }
+        );
+      })}
     </div>
-  )
+  );
 }
 
 export default AllArtList;
