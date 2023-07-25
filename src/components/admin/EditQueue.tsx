4e6d3767ffcca8ee28015448a99f41326b024891
Emma Gerigscott <elgerig@gmail.com>
2023-07-25 10:49:29 -0700

    add useEffect to get timer

--- src/components/admin/EditQueue.tsx
+++ src/components/admin/EditQueue.tsx
@@ -1,13 +1,43 @@
 import { QueueObj } from "./AdminControl";
-// Function will take all art in queue, show a list, and allow admin to reorder queue order.
-// Each item in the queue will have a queue number that is updated with this action
+import { db } from "../../firebase";
+import { ref, onValue, push, set } from "firebase/database";
+import { useState, useEffect } from 'react';
+
 interface QueueProps {
   queue: Array<QueueObj>
 }
 
+
 function EditQueue(props: QueueProps) {
+  const [timer, setTimer] = useState<number>(0)
+  let timerInterval: number;
+
+  useEffect(() => {
+    const timerdb = ref(db, 'timer');
+    const unSubscribe = onValue(
+      timerdb, (snapshot: import("firebase/database").DataSnapshot) => {
+        const data = snapshot.val() as number;
+        setTimer(data);
+      },
+      (error) => {
+        console.log(error)
+      }
+    );
+    return () => unSubscribe();
+  }, []);
+
+  const startTimerClick = () => {
+    if (!timerInterval && timer > 0) {
+      timerInterval = setInterval(() => {
+        setTimer(timer - 1)
+      }, 1000)
+    }
+  }
+
   return (
     <div className="queue">
+      <button>Start Timer</button>
+      <button>Stop Timer</button>
       <button>Edit Queue</button>
       {props.queue.map((art) => {
         return (
