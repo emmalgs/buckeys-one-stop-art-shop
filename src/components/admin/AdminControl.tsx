30511178cc98d7b6fc3cbe79237ad616b6f9b618
Emma Gerigscott <elgerig@gmail.com>
2023-07-25 13:52:25 -0700

    remove queue

--- src/components/admin/AdminControl.tsx
+++ src/components/admin/AdminControl.tsx
@@ -2,41 +2,29 @@ import { useEffect, useState } from 'react';
 import { ref, onValue, push, set } from "firebase/database";
 import { db, auth } from "../../firebase";
 import AdminLogin from './AdminLogin';
-import ArtQueueForm from './ArtQueueForm';
+import ArtQueueForm from './AddArtForm';
 import AdminHeader from './AdminHeader';
 import AdminLogout from './AdminLogout';
 import ArtDetails from './ArtDetails';
 import AllArtList from './AllArtList';
-import EditQueue from './EditQueue';
+import EditArtForm from './EditArtForm';
 
-interface ArtObj {
+export interface ArtObj {
   title: string;
   description: string;
   price: string;
   imageUrl: string;
   id: string;
-}
-
-export interface QueueObj {
-  id: string;
-  title: string;
-  description: string;
-  price: string;
-  imageUrl: string;
-  queueNumber: number;
-  timer: number;
-  available: boolean;
 }
 
 function AdminControl() {
   const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
   const [artList, setArtList] = useState<ArtObj[]>([]);
-  const [queueList, setQueueList] = useState<QueueObj[]>([]);
   const [selectedArt, setSelectedArt] = useState<ArtObj | null>(null);
   const [loginView, setLoginView] = useState(false);
   const [logoutView, setLogoutView] = useState(false);
-  const [viewQueue, setViewQueue] = useState(false);
   const [allArtView, setAllArtView] = useState(false);
+  const [editing, setEditing] = useState(false);
 
   useEffect(() => {
     const artdb = ref(db, 'art/');
@@ -58,29 +46,6 @@ function AdminControl() {
     (error) => {
       console.log(error);
     });
-    return () => unSubscribe();
-  }, []);
-
-  useEffect(() => {
-    const queuedb = ref(db, 'queue');
-    const unSubscribe = onValue(
-      queuedb, (snapshot: import("firebase/database").DataSnapshot) => {
-        const artworks: QueueObj[] = [];
-        const data = snapshot.val() as Record<string, QueueObj>;
-        const keys = Object.keys(data)
-        keys.forEach((art) => {
-          const artwork = {
-            ...data[art],
-          }
-          artworks.push(artwork);
-        });
-        setQueueList(artworks)
-        console.log(artworks)
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
     return () => unSubscribe();
   }, []);
 
@@ -114,74 +79,63 @@ function AdminControl() {
     }, 1000);
   }
 
-  const handleAddToQueue = (id: string) => {
-    const selection = artList.filter((artwork) => artwork.id === id)[0]
-    const queueItem: QueueObj = {
-      ...selection,
-      queueNumber: queueList.length + 1,
-      timer: 2000,
-      available: true
-    }
-    const newDataRef = push(ref(db, 'queue'));
-    set(newDataRef, queueItem)
-      .then(() => {
-        console.log('added to queue!')
-      })
-      .catch((error: {message: string }) => {
-        console.log(`error! : ${error.message}`)
-      });
-  }
-
-
-
   const handleLoginViewClick = () => {
     setLoginView(true);
     setFormVisibleOnPage(false);
     setLogoutView(false);
-    setViewQueue(false);
     setFormVisibleOnPage(false);
     setSelectedArt(null);
     setAllArtView(false);
+    setEditing(false);
   }
 
   const handleLogoutViewClick = () => {
     setLoginView(false);
     setFormVisibleOnPage(false);
     setLogoutView(true);
-    setViewQueue(false);
     setFormVisibleOnPage(false);
     setSelectedArt(null);
     setAllArtView(false);
+    setEditing(false);
   }
 
   const handleViewQueueClick = () => {
     setLoginView(false);
     setFormVisibleOnPage(false);
     setLogoutView(false);
-    setViewQueue(true);
     setFormVisibleOnPage(false);
     setSelectedArt(null);
     setAllArtView(false);
+    setEditing(false);
   }
 
   const handleAddArtClick = () => {
     setLoginView(false);
     setFormVisibleOnPage(false);
     setLogoutView(false);
-    setViewQueue(false);
     setFormVisibleOnPage(true);
     setSelectedArt(null);
     setAllArtView(false);
+    setEditing(false);
   }
 
   const handleViewAllArtClick = () => {
     setLoginView(false);
     setFormVisibleOnPage(false);
     setLogoutView(false);
-    setViewQueue(false);
     setFormVisibleOnPage(false);
     setSelectedArt(null);
     setAllArtView(true);
+    setEditing(false);
+  }
+
+  const handleViewEditingClick = () => {
+    setLoginView(false);
+    setFormVisibleOnPage(false);
+    setLogoutView(false);
+    setFormVisibleOnPage(false);
+    setAllArtView(false);
+    setEditing(true);
   }
 
 
@@ -197,9 +151,7 @@ function AdminControl() {
         <AdminHeader 
           loginClick={handleLoginViewClick} 
           logoutViewClick={handleLogoutViewClick} 
-          viewQueueClick={handleViewQueueClick}
-          viewAllArtClick={handleViewAllArtClick}
-          addArtClick={handleAddArtClick} />
+          viewAllArtClick={handleViewAllArtClick} />
         {currentView}
       </div>
     )
@@ -218,20 +170,20 @@ function AdminControl() {
       currentView = 
         <AllArtList 
           allArt={artList} 
-          queue={queueList}
           onArtClick={handleSelectArtClick} 
-          onAddToQueueClick={handleAddToQueue} />
-    } else if (viewQueue) {
-      currentView = <EditQueue queue = {queueList}/>
+          onAddArtClick={handleAddArtClick} />
+    } else if (editing) {
+      currentView = 
+        <EditArtForm
+          artwork={selectedArt}
+          updateArt={}
     }
     return (
       <div className='admin-body'>
         <AdminHeader 
           loginClick={handleLoginViewClick}
           logoutViewClick={handleLogoutViewClick}
-          viewQueueClick={handleViewQueueClick}
-          viewAllArtClick={handleViewAllArtClick}
-          addArtClick={handleAddArtClick} />
+          viewAllArtClick={handleViewAllArtClick} />
         {currentView}
       </div>
     )
