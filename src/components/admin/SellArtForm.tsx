5a7a5e3cbf4a9c4cff191f3f85f1c5b14eca29f0
Emma Gerigscott <elgerig@gmail.com>
2023-07-25 15:03:11 -0700

    create sellArtForm with details and form

---
+++ src/components/admin/SellArtForm.tsx
@@ -0,0 +1,43 @@
+import { ArtObj } from "./AdminControl";
+
+interface SellArtProps {
+  selection: ArtObj;
+  sellArt: (artwork: ArtObj, date: Date) => void;
+}
+
+function SellArtForm(props: SellArtProps) {
+  const art: ArtObj = props.selection;
+  // const currentDate: string = new Date().toISOString().split('.')[0];
+
+  const sellArtOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      date: { value: Date }
+    };
+    props.sellArt(art, target.date.value);
+  };
+  return (
+    <div className="sell-art-form">
+      <div className="selected-art">
+        <h2>Selected Art To Sell:</h2>
+        <div className="art-details">
+          <h2>{art.title}</h2>
+          <p>{art.description}</p>
+          <p>${parseInt(art.price).toFixed(2)}</p>
+          <p>
+            <img src={art.imageUrl} />
+          </p>
+        </div>
+      </div>
+      <div className="date-select">
+        <h2>Choose A Date To Close the Sale:</h2>
+        <form onSubmit={sellArtOnSubmit}>
+          <input type="datetime-local" name="date" />
+          <button type="submit">Sell It!</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SellArtForm;
