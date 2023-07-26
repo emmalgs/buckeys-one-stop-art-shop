80111424c8550776405710f1752c7de07c6a35e8
Emma Gerigscott <elgerig@gmail.com>
2023-07-26 11:37:43 -0700

    style cart add checkout btn

--- src/components/user/Cart.tsx
+++ src/components/user/Cart.tsx
@@ -8,24 +8,29 @@ interface CartProps {
 
 function Cart(props: CartProps) {
   return (
-    <div>
+    <div className="cart-main">
       <h1>Cart</h1>
+      <div className="line1"></div>
       {props.cartItems.length === 0 ? (
-        <p>Nothing in here!</p>
+        <p className="cart-item">Nothing in here!</p>
       ) : (
         props.cartItems.map((art) => {
           return (
             <div className="cart-item">
+              <div className="cart-item-info">
                 <img src={art.imageUrl} />
                 <span>{art.title} --</span>
-                <span>{art.description} --</span>
                 <span>${parseInt(art.price).toFixed(2)}</span>
-                <button onClick={() => props.delete(art)}>Delete From Cart</button>
+              </div>
+              <button onClick={() => props.delete(art)}>
+                Delete From Cart
+              </button>
             </div>
           );
         })
       )}
-      <h2>Cart Total: ${props.total.toFixed()}</h2>
+      <h2 className="total">Cart Total: <span>${props.total.toFixed(2)}</span></h2>
+      <button className="checkout">Checkout</button>
     </div>
   );
 }
