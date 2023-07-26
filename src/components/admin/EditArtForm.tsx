8b57ab8cd57ebba65cfb75c91817cb2621932d24
Emma Gerigscott <elgerig@gmail.com>
2023-07-26 09:10:04 -0700

    add image edit feature to edit form

--- src/components/admin/EditArtForm.tsx
+++ src/components/admin/EditArtForm.tsx
@@ -1,4 +1,6 @@
 import { ArtObj } from "./AdminControl";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { useState } from "react";
 
 interface FormProps {
   updateArt: (artwork: ArtObj) => void;
@@ -6,9 +8,36 @@ interface FormProps {
   artwork: ArtObj;
 }
 
+let uploadedImg: File | null = null;
+
 function EditArtForm(props: FormProps) {
+  const [imageURL, setImageURL] = useState<string | null>(
+    props.artwork.imageUrl
+  );
+  const [updateSuccess, setUpdateSuccess] = useState("");
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      uploadedImg = event.target.files[0];
+    }
+  };
+
+  const addImageToStorage = async () => {
+    if (!uploadedImg) {
+      setUpdateSuccess("No image selected");
+    } else {
+      const storage = getStorage();
+      const storageRef = ref(storage, `images/${uploadedImg.name}`);
+      await uploadBytes(storageRef, uploadedImg);
+
+      const url = await getDownloadURL(storageRef);
+      setImageURL(url);
+    }
+  };
+
   const updateArtOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
     const target = e.target as typeof e.target & {
       title: { value: string };
       description: { value: string };
@@ -19,15 +48,23 @@ function EditArtForm(props: FormProps) {
       title: target.title.value,
       description: target.description.value,
       price: target.price.value,
-      imageUrl: props.artwork.imageUrl,
+      imageUrl: imageURL,
       id: props.artwork.id,
     };
+
     props.updateArt(art);
+  };
+
+  const removeImage = () => {
+    setImageURL(null);
   };
 
   return (
     <div className="edit-art">
-      <p className="exit" onClick={() => props.backToArt(props.artwork.id)}>x</p>
+      <p className="exit" onClick={() => props.backToArt(props.artwork.id)}>
+        x
+      </p>
+      <p>{updateSuccess}</p>
       <h3>Editing {props.artwork.title}</h3>
       <form onSubmit={updateArtOnSubmit}>
         <label>Title</label>
@@ -38,7 +75,8 @@ function EditArtForm(props: FormProps) {
           defaultValue={props.artwork.title}
         />
         <label>Description</label>
-        <input type="text"
+        <input
+          type="text"
           name="description"
           placeholder="description of art piece"
           defaultValue={props.artwork.description}
@@ -50,7 +88,29 @@ function EditArtForm(props: FormProps) {
           placeholder="price"
           defaultValue={props.artwork.price}
         />
-        <button type="submit">Update Art</button>
+
+        {imageURL != null ? (
+          <>
+            <label>Image</label>
+            <img src={imageURL} />
+            <p onClick={removeImage}>Remove Image</p>
+          </>
+        ) : (
+          <>
+            <label htmlFor="file">Upload New Image: </label>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              id="file"
+            />
+            <img src={imageURL} />
+            <p onClick={addImageToStorage}>Add Image</p>
+          </>
+        )}
+        <button className="submit-edit" type="submit">
+          Update Art
+        </button>
       </form>
     </div>
   );
