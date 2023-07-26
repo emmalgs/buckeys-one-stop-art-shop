e4c310814281b60b2ad3dc23070cc0da3e1f5c9f
Emma Gerigscott <elgerig@gmail.com>
2023-07-26 11:14:12 -0700

    add delete from cart button

--- src/components/user/Cart.tsx
+++ src/components/user/Cart.tsx
@@ -3,6 +3,7 @@ import { SaleObj } from "../admin/AdminControl";
 interface CartProps {
   cartItems: Array<SaleObj>;
   total: number;
+  delete: (art: SaleObj) => void;
 }
 
 function Cart(props: CartProps) {
@@ -19,6 +20,7 @@ function Cart(props: CartProps) {
                 <span>{art.title} --</span>
                 <span>{art.description} --</span>
                 <span>${parseInt(art.price).toFixed(2)}</span>
+                <button onClick={() => props.delete(art)}>Delete From Cart</button>
             </div>
           );
         })
