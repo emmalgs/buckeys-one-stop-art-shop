131a09bc3354ce6986902be4f74243df3153c528
Emma Gerigscott <elgerig@gmail.com>
2023-07-26 14:12:08 -0700

    add style to page background

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <div className="background-circle"></div>
     <App />
   </React.StrictMode>,
 )
