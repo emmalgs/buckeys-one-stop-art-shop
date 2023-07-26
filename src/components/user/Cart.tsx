9da4c65d1fd36b9a1e7866c6b7830b4a1a5d9fba
Emma Gerigscott <elgerig@gmail.com>
2023-07-26 11:05:17 -0700

    add cart info and total

--- src/components/user/Cart.tsx
+++ src/components/user/Cart.tsx
@@ -1,9 +1,31 @@
-function Cart() {
+import { SaleObj } from "../admin/AdminControl";
+
+interface CartProps {
+  cartItems: Array<SaleObj>;
+  total: number;
+}
+
+function Cart(props: CartProps) {
   return (
-  <div>
-    <h1>Cart</h1>
-  </div>
-  )
+    <div>
+      <h1>Cart</h1>
+      {props.cartItems.length === 0 ? (
+        <p>Nothing in here!</p>
+      ) : (
+        props.cartItems.map((art) => {
+          return (
+            <div className="cart-item">
+                <img src={art.imageUrl} />
+                <span>{art.title} --</span>
+                <span>{art.description} --</span>
+                <span>${parseInt(art.price).toFixed(2)}</span>
+            </div>
+          );
+        })
+      )}
+      <h2>Cart Total: ${props.total.toFixed()}</h2>
+    </div>
+  );
 }
 
 export default Cart;
