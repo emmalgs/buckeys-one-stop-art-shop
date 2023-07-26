65631a516ece99d94f7622edbd43354edcada384
Emma Gerigscott <elgerig@gmail.com>
2023-07-26 11:05:47 -0700

    edit art info layout

--- src/components/user/ArtInfo.tsx
+++ src/components/user/ArtInfo.tsx
@@ -9,12 +9,10 @@ function ArtInfo(props: ArtInfo) {
   <div className="art-info">
       <h2 className="Title">{props.title}</h2>
     <div className="description">
-      <h2>Description</h2>
       <p>{props.description}</p>
     </div>
     <div className="price">
-      <h2>Price</h2>
-      <h3>${props.price}</h3>
+      <h3>${parseInt(props.price).toFixed(2)}</h3>
     </div>
   </div>
   )
