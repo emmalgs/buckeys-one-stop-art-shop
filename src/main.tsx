e535cc13052167736313e784f949cfea2b053a06
Emma Gerigscott <elgerig@gmail.com>
2023-07-27 11:23:24 -0700

    add gifs to webapge

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 import './index.css'
+import scurry from '../src/assets/img/scurry.gif';
+import dancer from '../src/assets/img/buckey_dance.gif'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className="background-circle"></div>
+    <div className='scurry'><img src={scurry} /></div>
+    <div className='dancer'><img src={dancer} /></div>
     <App />
   </React.StrictMode>,
 )
